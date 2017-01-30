b7a0fa0046513ae472844821cd68670076165a05
brodan <christopher.hranj@gmail.com>
2017-01-30 14:28:21 -0500

    install requests and cheerio dependencies and add new js file for task #1

---
+++ mapping.js
@@ -0,0 +1,5 @@
+/* steps: 
+    get html for page http://autofill.mozdev.org/autofilltest.html and grab all input names
+    extract autofill info from https://html.spec.whatwg.org/multipage/forms.html#autofill
+    map input names to autofill info?
+*/
