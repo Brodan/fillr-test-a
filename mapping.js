80f9d0d872109b48316f1e9f87547d76182186a9
brodan <christopher.hranj@gmail.com>
2017-01-30 14:59:33 -0500

    update mapping.js to retrieve html and parse input controls

--- mapping.js
+++ mapping.js
@@ -3,3 +3,15 @@
     extract autofill info from https://html.spec.whatwg.org/multipage/forms.html#autofill
     map input names to autofill info?
 */
+var request = require('request')
+var cheerio = require('cheerio');
+
+var url = 'http://autofill.mozdev.org/autofilltest.html';
+
+request(url, function(err, resp, body){
+  $ = cheerio.load(body);
+  inputs = $('input, select');
+  $(inputs).each(function(i, input){
+    console.log($(input).attr('name'));
+  });
+});
