6d52ad09c856836311e574d648983c934d9fdcee
brodan <christopher.hranj@gmail.com>
2017-01-31 16:01:56 -0500

    understand jsdom. task #1 finished

--- extract.js
+++ extract.js
@@ -1,21 +1,13 @@
-var request = require('request')
 var cheerio = require('cheerio');
 
 module.exports.extract = function(window) {
-  var output = { };
-  request('http://autofill.mozdev.org/autofilltest.html', function(err, resp, body){
-    if (!err && resp.statusCode == 200) {
-      $ = cheerio.load(body);
-      inputs = $('input, select');
-      $(inputs).each(function(i, input){
-        control_label = String($(input).parent().prev().html().trim());
-        name = String($(input).attr('name'));
-        output[control_label] = control_label + " " + name;
-      });
-    }
-    else {
-      console.log('error retrieving mozdev page.');
-    }
-    return output
-  });
+  var output = { };    
+    $ = cheerio.load(window.document.documentElement.outerHTML);
+    inputs = $('input, select');
+    $(inputs).each(function(i, input){
+      control_label = String($(input).parent().prev().html().trim());
+      name = String($(input).attr('name'));
+      output[control_label] = control_label + " " + name;
+    });
+  return output
 }
