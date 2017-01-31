c3f02df0b6e33dd0c26757781c4ce2a7f49729c9
brodan <christopher.hranj@gmail.com>
2017-01-31 15:21:11 -0500

    callback isnt working

--- extract.js
+++ extract.js
@@ -2,22 +2,20 @@ var request = require('request')
 var cheerio = require('cheerio');
 
 module.exports.extract = function(window) {
-  // Write your solution to Task #2 - Extract Metadata task here
-  var url = 'http://autofill.mozdev.org/autofilltest.html';
-
-  request(url, function(err, resp, body){
+  var output = { };
+  request('http://autofill.mozdev.org/autofilltest.html', function(err, resp, body){
     if (!err && resp.statusCode == 200) {
-        $ = cheerio.load(body);
-        inputs = $('input, select');
-        $(inputs).each(function(i, input){
-          // the key of each element in the hash will be the ‘name’ attribute of the control.
-          metadata = $(input).attr('name').prev();
-          //output[key] = ""; // to be filled in manually in mapping.json
-          console.log(metadata);
-        });
-      }
+      $ = cheerio.load(body);
+      inputs = $('input, select');
+      $(inputs).each(function(i, input){
+        control_label = String($(input).parent().prev().html().trim());
+        name = String($(input).attr('name'));
+        output[control_label] = control_label + " " + name;
+      });
+    }
     else {
       console.log('error retrieving mozdev page.');
     }
+    return output
   });
 }
