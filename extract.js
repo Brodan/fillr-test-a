7d295f74da1d7fecf20c7a1985fe2009b4c3d05b
brodan <christopher.hranj@gmail.com>
2017-01-31 22:57:58 -0500

    remove cheerio from extract.js

--- extract.js
+++ extract.js
@@ -3,23 +3,18 @@
  * extract parses through the Mozilla autofill test page
  * and extracts control_labls and attribute names and returns
  * a hash object in the required format. 
- * cheerio is used to parse through the jsdom window
- * object in a fashion similar to jquery.
  *
  * @summary   Extract metadata from Mozilla's autofill test page.
  *
  * @author    Christopher Hranj
  */
-var cheerio = require('cheerio');
-
 module.exports.extract = function(window) {
-  var output = { };
-    $ = cheerio.load(window.document.documentElement.outerHTML);
-    inputs = $('input, select');
-    $(inputs).each(function(i, input){
-      control_label = String($(input).parent().prev().html().trim());
-      name = String($(input).attr('name'));
-      output[control_label] = control_label + " " + name;
-    });
+  var output = {};
+  inputs = window.document.documentElement.querySelectorAll('input, select');
+  for (var i = 0; i < inputs.length; i++) {
+    control_label = String(inputs[i].parentNode.previousSibling.innerHTML.trim());
+    name = String(inputs[i].name);
+    output[control_label] = control_label + " " + name;
+  }
   return output;
 }
